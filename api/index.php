6cb3cb1e3294584ec05ae5fb6486ce19a00ddbc0
Daniels SOARES <danielo-24@hotmail.com>
2025-01-24 13:53:56 +0100

    v1.1 test

--- api/index.php
+++ api/index.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta property="og:site_name" content="GameAssetFactory">
-    <meta property="og:title" content="&#x26CA; GameAssetFactory">
+    <meta property="og:site_name" content="GameAssetFactory 3D Creation">
+    <meta property="og:title" content="GameAssetFactory">
     <meta property="og:description" content="Build worlds, piece by piece">
-    <meta property="og:url" content="https://gameassetfactory.com/">
-    <meta property="og:image" content="../img/GameAssetFactoryLogo1024.png">
+    <meta property="og:url" content="https://gaf-website.vercel.app/">
+    <meta property="og:image" content="https://gaf-website.vercel.app/img/GameAssetFactoryLogo1024.png">
     <link rel="icon" href="../img/GameAssetFactoryLogopetit.ico">
     <title id="pageTitle"></title>
     
@@ -22,9 +22,6 @@
 
       
     />
-
-
-    
   </head>
 
 
