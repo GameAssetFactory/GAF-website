411271cf33f0c4cea703c446c573707e21cf28f5
Daniels SOARES <danielo-24@hotmail.com>
2025-01-24 17:34:20 +0100

    TEST

--- api/home.php
+++ api/home.php
@@ -36,6 +36,94 @@
           </div>
         </div>
 
+
+        <div class="home__content grid">
+            <div class="home__social">
+
+            </div>
+            <div class="home__data">
+              <h1 class="home__title">Hey, Je m'appelle Daniels Soares</h1>
+              <h3 class="home__subtitle">
+                Dev d'app / Site Web
+              </h3>
+              <p class="home__description">
+                Il y aura toutes les infos me concernant, dans ce portfolio
+                <br><br><strong>Lycée Renée Cassin </strong>, BTS SIO SLAM.
+              </p>
+              <a href="#Me-Contacter"               class="button button--flex button--small portfolio__button">
+                Contactez-moi <i class="uil uil-message button__icon"></i>
+              </a>
+            </div>
+          </div>
+
+
+
+
+          <div class="home__content grid">
+            <div class="home__social">
+
+            </div>
+            <div class="home__data">
+              <h1 class="home__title">Hey, Je m'appelle Daniels Soares</h1>
+              <h3 class="home__subtitle">
+                Dev d'app / Site Web
+              </h3>
+              <p class="home__description">
+                Il y aura toutes les infos me concernant, dans ce portfolio
+                <br><br><strong>Lycée Renée Cassin </strong>, BTS SIO SLAM.
+              </p>
+              <a href="#Me-Contacter"               class="button button--flex button--small portfolio__button">
+                Contactez-moi <i class="uil uil-message button__icon"></i>
+              </a>
+            </div>
+          </div>
+
+
+
+
+          <div class="home__content grid">
+            <div class="home__social">
+
+            </div>
+            <div class="home__data">
+              <h1 class="home__title">Hey, Je m'appelle Daniels Soares</h1>
+              <h3 class="home__subtitle">
+                Dev d'app / Site Web
+              </h3>
+              <p class="home__description">
+                Il y aura toutes les infos me concernant, dans ce portfolio
+                <br><br><strong>Lycée Renée Cassin </strong>, BTS SIO SLAM.
+              </p>
+              <a href="#Me-Contacter"               class="button button--flex button--small portfolio__button">
+                Contactez-moi <i class="uil uil-message button__icon"></i>
+              </a>
+            </div>
+          </div>
+
+
+
+
+          <div class="home__content grid">
+            <div class="home__social">
+
+            </div>
+            <div class="home__data">
+              <h1 class="home__title">Hey, Je m'appelle Daniels Soares</h1>
+              <h3 class="home__subtitle">
+                Dev d'app / Site Web
+              </h3>
+              <p class="home__description">
+                Il y aura toutes les infos me concernant, dans ce portfolio
+                <br><br><strong>Lycée Renée Cassin </strong>, BTS SIO SLAM.
+              </p>
+              <a href="#Me-Contacter"               class="button button--flex button--small portfolio__button">
+                Contactez-moi <i class="uil uil-message button__icon"></i>
+              </a>
+            </div>
+          </div>
+
+
+
         <br><br><br><br><br><br>
       </section>  
 
