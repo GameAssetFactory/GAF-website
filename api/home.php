893bc8bf4b4898dfbd2e5a1e93ff39d5a1db05dd
Daniels SOARES <danielo-24@hotmail.com>
2025-01-30 11:53:06 +0100

    V1.0.6
    fix css for jobs and contact
    email form
    
    fix home page
    added images

--- api/home.php
+++ api/home.php
@@ -15,39 +15,10 @@
             <div class="home__social">
               <!-- Vous pouvez ajouter des liens sociaux ici si nécessaire -->
             </div>
-            <div class="home__img">
-              <svg
-                class="home__blob"
-                viewBox="0 0 200 187"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-              >
-                <mask id="mask0" mask-type="alpha">
-                  <path
-                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-                      130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-                      97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-                      0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                  />
-                </mask>
-                <g mask="url(#mask0)">
-                  <path
-                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-                      165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-                      129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-                     -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                  />
-                  <image
-                    class="home__blob-img"
-                    x="-79"
-                    y="-46"
-                    xlink:href="../img/homefond.png"
-                  />
-                </g>
-              </svg>
-            </div>
             <div class="home__data">
-              <h1 class="home__title">GAME ART STUDIO</h1>
+              <div class="home__logo">
+                <img src="../img/GameAssetFactoryLogo1024.png" alt="Game Asset Factory" class="home__logo-img">
+              </div>
               <h3 class="home__subtitle">
                 Video Games, Architectural visualization and Renders
               </h3>
@@ -71,10 +42,84 @@
 
       </section>
 
+
+      <!--========================= Services =====================-->
+      <section class="services section" id="Services">
+      <h2 class="section__title">OUR SERVICES</h2>
+      <div class="services__container container grid">
+          <div class="services__content">
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-wrench services__icon"></i>
+                <h3 class="services__title">Custom work</h3>
+              </div>
+              <p class="services__description">
+                If you want extra models, materials, textures or whatever for an existing pack, you can contact us via the "contact" page. Game Asset Factory is also available for on-demand work; it can be full environments, scenes, unique 3D models, materials, textures or functionality for your project. Every asset produced will fit with your artistic and technical direction.
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/Custom_Work.jpg');"></div>
+            </div>
+
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-box services__icon"></i>
+                <h3 class="services__title">Ready-to-use packs</h3>
+              </div>
+              <p class="services__description">
+                Our packs provide ready-to-use assets which are grouped by theme or functionality. We focus on giving complete solutions in order to prevent from additional work for our customers.
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/Castle.jpg');"></div>
+            </div>
+
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-cube services__icon"></i>
+                <h3 class="services__title">3D assets</h3>
+              </div>
+              <p class="services__description">
+                Our ready-to-use 3D models are made to be integrated easily in any project. They are also meant to be customizable and resource-friendly. Every model come with its own materials, textures, LODs (Level of details) and collision. We offer a wide variety of models!
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/3D_Assets.jpg');"></div>
+            </div>
+
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-mountains services__icon"></i>
+                <h3 class="services__title">Environments</h3>
+              </div>
+              <p class="services__description">
+                From stylized to realistic environments, Game Asset Factory provides solutions for every situation, using Hard-surface, organic modeling or Photogrammetry technique. Environments are generally composed of different asset types, like landscapes, 3D models, materials (shaders), textures, etc. This way, you can use pre-made scenes or create your own with our quality assets.
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/Environment.jpg');"></div>
+            </div>
+
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-palette services__icon"></i>
+                <h3 class="services__title">Materials</h3>
+              </div>
+              <p class="services__description">
+                We like to keep things simple and powerful. Our process is to create one or several master materials that will be reused through different instances. Master materials are designed in a clear way with annotations and explicit descriptions.
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/Materials.jpg');"></div>
+            </div>
+
+            <div class="services__card">
+              <div class="services__header">
+                <i class="uil uil-comments services__icon"></i>
+                <h3 class="services__title">Support</h3>
+              </div>
+              <p class="services__description">
+                Quality is assured by our support through corrections, upgrades and suggestions. Feel free to contact us if you have any question or suggestion.
+              </p>
+              <div class="services__background" style="background-image: url('../img/Home/Happy_Hotline.png');"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
       <!--========================= About Us =====================-->
       <section class="about section" id="About">
-      <br><br>
-
         <h2 class="section__title">ABOUT US</h2>
         <div class="about__container container grid">
           <div class="about__content">
@@ -120,77 +165,6 @@
         </div>
 
         
-      </section>
-
-
-      <!--========================= Services =====================-->
-      <section class="services section" id="Services">
-        <h2 class="section__title">OUR SERVICES</h2>
-        <div class="services__container container">
-          <div class="services__content">
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-wrench services__icon"></i>
-                <h3 class="services__title">Custom work</h3>
-              </div>
-              <p class="services__description">
-                If you want extra models, materials, textures or whatever for an existing pack, you can contact us via the "contact" page. Game Asset Factory is also available for on-demand work; it can be full environments, scenes, unique 3D models, materials, textures or functionality for your project. Every asset produced will fit with your artistic and technical direction.
-              </p>
-            </div>
-
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-box services__icon"></i>
-                <h3 class="services__title">Ready-to-use packs</h3>
-              </div>
-              <p class="services__description">
-                Our packs provide ready-to-use assets which are grouped by theme or functionality. We focus on giving complete solutions in order to prevent from additional work for our customers.
-              </p>
-              <div class="services__background" style="background-image: url('../img/Gallery/1.png');"></div>
-            </div>
-
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-cube services__icon"></i>
-                <h3 class="services__title">3D assets</h3>
-              </div>
-              <p class="services__description">
-                Our ready-to-use 3D models are made to be integrated easily in any project. They are also meant to be customizable and resource-friendly. Every model come with its own materials, textures, LODs (Level of details) and collision. We offer a wide variety of models!
-              </p>
-            </div>
-
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-mountains services__icon"></i>
-                <h3 class="services__title">Environments</h3>
-              </div>
-              <p class="services__description">
-                From stylized to realistic environments, Game Asset Factory provides solutions for every situation, using Hard-surface, organic modeling or Photogrammetry technique. Environments are generally composed of different asset types, like landscapes, 3D models, materials (shaders), textures, etc. This way, you can use pre-made scenes or create your own with our quality assets.
-              </p>
-            </div>
-
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-palette services__icon"></i>
-                <h3 class="services__title">Materials</h3>
-              </div>
-              <p class="services__description">
-                We like to keep things simple and powerful. Our process is to create one or several master materials that will be reused through different instances. Master materials are designed in a clear way with annotations and explicit descriptions.
-              </p>
-            </div>
-
-            <div class="services__card">
-              <div class="services__header">
-                <i class="uil uil-comments services__icon"></i>
-                <h3 class="services__title">Support</h3>
-              </div>
-              <p class="services__description">
-                Quality is assured by our support through corrections, upgrades and suggestions. Feel free to contact us if you have any question or suggestion.
-              </p>
-              <div class="services__background" style="background-image: url('../img/Gallery/Happy_Hotline.png');"></div>
-            </div>
-          </div>
-        </div>
       </section>
 
 
